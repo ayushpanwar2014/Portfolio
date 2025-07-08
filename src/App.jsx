08743a7f10cd854bad3949a6dd8126e4c759f5a5
ayushpanwar2014 <ayush.panwar2014@gmail.com>
2025-07-08 13:31:10 +0530

    deleted items

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,6 @@ import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
 import Parallax from "./components/parallax/Parallax";
 import Portfolio from "./components/portfolio/Portfolio";
-import Services from "./components/services/Services";
-import Test from "./Test.jsx"
-import React from "react";
 
 
 const App = () => {
